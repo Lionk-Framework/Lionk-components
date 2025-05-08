f3aae6066370ce2b8df6e3f43f0c65f3e05650e8
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2025-05-08 14:19:50 +0200

    feat: add BaseTemperatureSensor

---
+++ Temperature/TemperatureType.cs
@@ -0,0 +1,24 @@
+﻿// Copyright © 2024 Lionk Project
+
+namespace Lionk.Components.Temperature;
+
+/// <summary>
+/// The type of the temperature.
+/// </summary>
+public enum TemperatureType
+{
+    /// <summary>
+    /// The temperature is in Celsius.
+    /// </summary>
+    Celsius = 0,
+
+    /// <summary>
+    /// The temperature is in Fahrenheit.
+    /// </summary>
+    Fahrenheit = 1,
+
+    /// <summary>
+    /// The temperature is in Kelvin.
+    /// </summary>
+    Kelvin = 2,
+}
